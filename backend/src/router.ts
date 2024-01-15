b52c8af77fce3c87eac45240c1b5cee128556c43
Jeremias Regiani <jeremias.farizeli@gmail.com>
2024-01-15 09:29:11 -0300

    up

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -2,13 +2,12 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ ok: true})
+router.get("/teste", (req: Request, res: Response) => {
+  return res.json({ ok: true });
 });
 
-router.get('/', (req: Request, res: Response) => {
-    throw new Error('Erro ao fazer essa requisição....')  
+router.get("/", (req: Request, res: Response) => {
+  return res.json({ ok: true });
 });
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,36 +1,36 @@
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors'
-import cors from 'cors'
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
 
 import { router } from "./router";
-const porta: number = 8000;
+const porta: number = 3333;
 
 const app = express();
-app.use(express.json);
-app.use(cors())
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send('Algo deu errado!');
-});
 app.use(router);
+app.use(express.json);
+app.use(cors());
 
-
-// Esse é um midleware de tratativa de erros em rotas, toda rota vai passar por ele. 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-        // Se for uma instancia de um erro vamos lançar uma exceçção
-        return res.status(400).json({
-            error: err.message
-        })
-    }
+  console.error(err.stack);
+  res.status(500).send("Algo deu errado!");
+});
 
-    return res.status(500).json({
-        status: 'error',
-        message: 'Internal server error.'
-    })
-})
+// Esse é um midleware de tratativa de erros em rotas, toda rota vai passar por ele.
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    // Se for uma instancia de um erro vamos lançar uma exceçção
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error.",
+  });
+});
 
 app.listen(porta, () => {
-    console.log(`Servidor rodando na porta ${porta}`);
+  console.log(`Servidor rodando na porta ${porta}`);
 });
